bfa8dad1e9dbe4cf546002f7a84d900f97d663c4
柴华君 <chaihj15@mails.tsinghua.edu.cn>
2018-04-06 22:18:46 +0800

    wordcloud complete

--- hw2/hw2.R
+++ hw2/hw2.R
@@ -1,10 +1,13 @@
 #!/usr/bin/env Rscript
-library("XML")
+library("XML")  #load xml
 library("methods")
 library("tm")
-#library("qdap")  #stem
 library("parallel")
-library("SnowballC")
+library("SnowballC")  #stem word
+library("wordcloud")  #word cloud
+library("RColorBrewer")
+library("ggplot2")
+library("NLP")
 
 cleanFun <- function(xmlString){  #remove tag
   return (gsub("<.*?>", "", xmlString))
@@ -31,14 +34,26 @@ stem_text <- function(text, language = "porter", mc.cores = 1){  #stem word func
   return(unlist(x))
 }
 
+data <- function(dt){
+  word <- names(dt)
+  count <- c()
+  for(i in 1:length(dt)){
+    count[i] <- dt[[i]]
+  }
+  wordcloud <- data.frame(word, count)
+  return(wordcloud)
+}
+
 file = list.files("./samples_500/")
 file_num = length(file)
 my.dataframe <- data.frame("title" = character(),  "year" = character(), "month" = character(), "day" = character(), "Attr"=character(), "full_text"=character())
 word_set <- list()
+text_vector <- c()
+full.text <- c()
 
 for(i in 1:file_num){
-  if(i == 6)
-    break
+  if(i == 30)
+     break
   doc_name = paste("./samples_500/", file[i], sep = "")
   doc <- xmlParse(doc_name)  #loading xml file
   text_node = getNodeSet(doc, "//block[@class='full_text']")  #get full_text
@@ -64,7 +79,7 @@ for(i in 1:file_num){
   text_vector = gsub("^\\s+|\\s+$", "", text_vector)  #remove head and tail space
   text_vector = gsub("\\s+", " ", text_vector)  #remove more space 
   stem_news = stem_text(text_vector, language = 'en')  #stem the news
-
+  full.text <- c(full.text, stem_news)
   word <- strsplit(stem_news, split=" ")  #split word  
   unlist_word <- unlist(word)
   word_set <- c(word_set, list(unlist_word))
@@ -93,22 +108,43 @@ for(i in 1:file_num){
   month = sapply(month_node, xmlGetAttr, "content")
   day_node = getNodeSet(doc, "//meta[@name='publication_day_of_month']")
   day = sapply(day_node, xmlGetAttr, "content")
-  
   my.dataframe <- rbind(my.dataframe, data.frame( year, month, day, AttrVector, stem_news))  #first question
 }
+full.text <- paste(full.text, collapse = " ")
 
 unlist_set <- unlist(word_set)  #bulid Bag Of Word Vector
 unique_word_set <- unique(unlist_set)  #Duplicate removal
 bag_of_word <- list()
-for(i in 1 : 5){
+for(i in 1 : 30){
   vector_num <- c()
   single_set <- unlist(word_set[i])  #the words of signal news
   for(j in 1 : length(unique_word_set)){ 
     patter <- paste("^", unique_word_set[j], "$", sep = "");  #match unique word in signal news
     occur_num <- length(grep(patter, single_set))  #get the number of occur
-    #print(occur_num)
     vector_num <- c(vector_num, occur_num)
   }
   bag_of_word <- c(bag_of_word, list(vector_num))
 }
-print(bag_of_word)
+
+freq_word <- unlist(bag_of_word[1])
+freq.key <- list()
+freq.dataframe <- data.frame("word" = character(), "freq" = character())
+
+
+for(i in 2 : 30)   #get word cloud
+  freq_word <- c(freq_word) + c(unlist(bag_of_word[i]))
+words.order <- order(freq_word, decreasing=TRUE)[1:100]  #find the index of the max elements 
+keyword <- c()
+for(i in 1 : 100){
+  keyword <- c(keyword, unique_word_set[words.order[i]])
+}
+keyword <- paste(keyword, collapse = " ")
+print(keyword)
+wordcloud(full.text, max.word = 100, random.order=FALSE)
+
+
+
+
+
+
+
